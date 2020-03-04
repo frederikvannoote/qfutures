3785150c673ec171bdc3863ec8f394a20f6fcd47
frederik vannoote <frederik.vannoote@barco.com>
2020-03-04 22:07:21 +0100

    Try to increase threads in threadpool.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <QCoreApplication>
+#include <QThreadPool>
+#include <QDebug>
 #include "myclass.h"
 
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "Active threads:" << QThreadPool::globalInstance()->activeThreadCount();
+
+    QThreadPool::globalInstance()->setMaxThreadCount(50);
 
     MyClass obj1;
     obj1.start();
